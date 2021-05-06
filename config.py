0c4a3e33d331fc75e30534d302e67a59995829ed
Catalin Stroe <alexandru.catalin.stroe@gmail.com>
2021-05-06 12:54:32 +0300

    SecretBot

--- config.py
+++ config.py
@@ -1,6 +1,11 @@
+import environ
+
+env = environ.Env(SECRET_KEY = str,)
+
+
 # Can be multiple prefixes, like this: ("!", "?")
 BOT_PREFIX = ("$htf " , "htf-")
-TOKEN = "ODMwNzQ4MDM3MjkwNzg2ODY3.YHLMZg.DDw48-s8ZSIbPMDi5QxGSh1bCwI"
+TOKEN = env('SECRET_TOKEN')
 APPLICATION_ID = "830748037290786867"
 OWNERS = [104005181103964160, 437404402781782028]
 BLACKLIST = []
